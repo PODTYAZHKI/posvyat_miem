c102b0242f9f2f1f39488574590c67a597bab6ea
Sergey Muzychuk <supper.ser2001@gmail.com>
2022-08-21 03:31:10 +0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,60 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar style="padding-left: 160px; padding-right: 160px;">
+        <q-avatar size="60px" class="q-my-md">
+          <img src="src/assets/logo.svg">
+        </q-avatar>
+        <q-space />
+        <div class="links flex">
+          <a v-for="link in menu_links" :key="link.name" class="q-px-md text-accent" cursor-pointer>
+            {{ link.field }}
+          </a>
+        </div>
+        <!-- <q-toolbar-title>Quasar Framework</q-toolbar-title> -->
+        <q-space />
+        <q-btn color="secondary" icon="img:src/assets/vk.png" padding="10px 10px" class="q-mr-md"/>
+        <q-btn color="secondary" icon="img:src/assets/telegram.png" padding="10px 10px" />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+// import { defineComponent, ref } from 'vue'
+// import EssentialLink from 'components/EssentialLink.vue'
+const menu_links = [
+  {
+    name: 'about',
+    field: 'О нас'
+  },
+  {
+    name: 'Cost',
+    field: 'Стоимость'
+  },
+  {
+    name: 'Partners',
+    field: 'Партнёры'
+  },
+  {
+    name: 'Galary',
+    field: 'Фото'
+  }
+]
+
+
+
+</script>
+<style scoped lang="scss">
+.btn {
+  &-link {
+    //background: white;
+    //width: 66px;
+    //height: 66px;
+  }
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,94 @@
+<template>
+  <q-page class="flex flex-center" color="primary" style="padding-left: 160px; padding-right: 160px;">
+
+
+    <q-img src="src/assets/background-1.png" class="background" />
+    <div class="information flex flex-center text-accent">
+      <div class="suptitle q-mb-md">
+        1-2 Октября
+      </div>
+      <q-img src="src/assets/title.png" class="title q-mb-md" />
+      <div class="subtitle flex flex-center column">
+        Скорее регистрируйся и проведи <br> лучшую ночь этой осени вместе с нами!
+        <div class="q-mt-md">
+          <q-btn no-caps label="Регистрация" class="btn btn-reg q-mr-md" size="32px"/>
+          <q-btn no-caps label="Узнать больше" class="btn btn-more"  />
+        </div>
+      </div>
+    </div>
+    <!-- <div class="lines">
+      <q-img src="src/assets/line-purple.png" />
+      <q-img src="src/assets/line-yellow.png" />
+    </div> -->
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
+<style lang="scss" scoped>
+
+
+.background {
+  position: absolute;
+  top: 0;
+  z-index: -1;
+}
+
+.information {
+  width: 100%;
+  z-index: 1;
+}
+
+.suptitle {
+  font-style: normal;
+  font-weight: 800;
+  font-size: 40px;
+  text-transform: uppercase;
+  font-feature-settings: 'pnum' on, 'lnum' on;
+}
+
+.subtitle {
+  font-weight: 600;
+  font-size: 40px;
+  line-height: 100%;
+  /* or 40px */
+
+  text-align: center;
+  font-feature-settings: 'pnum' on, 'lnum' on;
+}
+.btn:hover {
+  cursor: pointer;
+}
+.btn {
+  border-radius: 16px;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 32px;
+  line-height: 100%;
+  /* identical to box height, or 32px */
+
+  font-feature-settings: 'pnum' on, 'lnum' on;
+  padding-right: 50px;
+  padding-left: 50px;
+
+  &-reg {
+    background: #EE4D07;
+    color: #fff;
+
+  }
+
+  &-more {
+    border: 4px solid #EE4D07;
+    color: #EE4D07;
+  }
+}
+.lines {
+  width: 100vw;
+}
+</style>
