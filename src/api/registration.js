4917c208a986baa854eb88b408a86f045b834480
Sergey Muzychuk <supper.ser2001@gmail.com>
2022-09-06 17:51:45 +0300

    fix urls

--- src/api/registration.js
+++ src/api/registration.js
@@ -1,6 +1,6 @@
 import { api } from "boot/axios";
 export async function register(person) {
-  const url = `http://miemposvyat.ru/api/registration`;
+  const url = `http://miemposvyat.ru:8000/api/registration`;
 
   return await api
     .post(url, person)
