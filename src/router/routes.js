c33cd4bb83d3a17c208144a611c9bab948924dd3
Sergey Muzychuk <supper.ser2001@gmail.com>
2022-09-26 00:09:42 +0300

    comment resettlement

--- src/router/routes.js
+++ src/router/routes.js
@@ -11,13 +11,13 @@ const routes = [
       { path: "", component: () => import("pages/RegistrationPage.vue") },
     ],
   },
-  {
-    path: "/resettlement",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [
-      { path: "", component: () => import("pages/ResettlementPage.vue") },
-    ],
-  },
+  // {
+  //   path: "/resettlement",
+  //   component: () => import("layouts/MainLayout.vue"),
+  //   children: [
+  //     { path: "", component: () => import("pages/ResettlementPage.vue") },
+  //   ],
+  // },
 
   // Always leave this as last one,
   // but you can also remove it
